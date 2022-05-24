dd4f3aa0b130479ee6618f8d200872cdfaec8090
swellFella <guzman9nicholas@gmail.com>
2022-05-24 06:28:18 -0500

    Added adjustable grid Added CSS to body element Added CSS to #grid_container

--- scripts.js
+++ scripts.js
@@ -1,23 +1,29 @@
-const grid_container = document.getElementById('#grid_container');
-
 
 /**
- * function that creates recieves a number and makes that many divs
- * divs are appended to grid_container as a child element.
+ * Function that takes a number num and generates a grid size numXnum
+ * the grid then is filled with divs using a for-loop
  */
-function createDivs(num){
-    for(let i = 0; i < num; i++){
+function setGridSize(num){
+    // creates a variable called grid_size that points to the element with ID grid_container
+    grid_size = document.getElementById('grid_container');
+    // alters grid_container style attributes and set the number of repeated rows to function arg
+    grid_size.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+    grid_size.style.gridTemplateRows = `repeat(${num}, 1fr)`;
+
+    // After grid is created, for-loop fills area of grid with divs
+    for(let i = 0; i < (num*num); i++){
         const div = document.createElement('div');
         
-        div.style.height = '200px';
-        div.style.width = '200px';
-        div.style.border = '1px dashed black';
-        
-        div.style.textAlign = 'center';
-        div.textContent = 'JS div grid block';
-        
-        document.getElementById('grid_container').appendChild(div);
+        div.style.height = '10px';
+        div.style.width = '10px';
+        div.style.border = '1px dotted black';
+        div.style.margin = '0px';
+        div.style.padding = '0px';
+
+        document.getElementById('grid_container').appendChild(div);        
     }
+
+
 }
 
-createDivs(16);
+setGridSize(20);
