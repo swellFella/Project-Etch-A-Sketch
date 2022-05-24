27b9c848b65b561c8b096a56ce7c0fd46eecf04c
swellFella <guzman9nicholas@gmail.com>
2022-05-24 04:13:40 -0500

    Added JS function for creating divs

--- scripts.js
+++ scripts.js
@@ -1,19 +1,10 @@
 const grid_container = document.getElementById('#grid_container');
 
-// for-loop to create 16 divs
-// for(let i = 0; i < 16; i++){
-//     const div = document.createElement('div');
-    
-//     div.style.height = '200px';
-//     div.style.width = '200px';
-//     div.style.border = '1px dashed black';
-    
-//     div.style.textAlign = 'center';
-//     div.textContent = 'JS div grid block';
-    
-//     document.getElementById('grid_container').appendChild(div);
-// }
 
+/**
+ * function that creates recieves a number and makes that many divs
+ * divs are appended to grid_container as a child element.
+ */
 function createDivs(num){
     for(let i = 0; i < num; i++){
         const div = document.createElement('div');
