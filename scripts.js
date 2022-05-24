e26bb086cd5da34813dca19992d5848967f95721
swellFella <guzman9nicholas@gmail.com>
2022-05-24 04:12:52 -0500

    Added JS function for creating divs

--- scripts.js
+++ scripts.js
@@ -1,15 +1,32 @@
 const grid_container = document.getElementById('#grid_container');
 
 // for-loop to create 16 divs
-for(let i = 0; i < 16; i++){
-    const div = document.createElement('div');
+// for(let i = 0; i < 16; i++){
+//     const div = document.createElement('div');
     
-    div.style.height = '200px';
-    div.style.width = '200px';
-    div.style.border = '1px dashed black';
+//     div.style.height = '200px';
+//     div.style.width = '200px';
+//     div.style.border = '1px dashed black';
     
-    div.style.textAlign = 'center';
-    div.textContent = 'JS div grid block';
+//     div.style.textAlign = 'center';
+//     div.textContent = 'JS div grid block';
     
-    document.getElementById('grid_container').appendChild(div);
+//     document.getElementById('grid_container').appendChild(div);
+// }
+
+function createDivs(num){
+    for(let i = 0; i < num; i++){
+        const div = document.createElement('div');
+        
+        div.style.height = '200px';
+        div.style.width = '200px';
+        div.style.border = '1px dashed black';
+        
+        div.style.textAlign = 'center';
+        div.textContent = 'JS div grid block';
+        
+        document.getElementById('grid_container').appendChild(div);
+    }
 }
+
+createDivs(16);
